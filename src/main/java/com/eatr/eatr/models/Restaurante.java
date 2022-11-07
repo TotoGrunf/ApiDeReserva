dd71554ae45f10344a05f8c11a62ce3239388bff
Alexis Galarza <colo1990.ag@gmail.com>
2022-11-06 23:11:30 -0300

    Creación de clase restaurante

--- src/main/java/com/eatr/eatr/models/Restaurante.java
+++ src/main/java/com/eatr/eatr/models/Restaurante.java
@@ -1,4 +1,88 @@
 package com.eatr.eatr.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
 public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    private String nombreResto, email, contraseña, ubicacion;
+    private int numMesas;
+    private LocalDateTime diasHorarios;
+
+    public Restaurante() {
+    }
+
+    public Restaurante(long id, String nombreResto, String email, String contraseña, String ubicacion, int numMesas, LocalDateTime diasHorarios) {
+        this.id = id;
+        this.nombreResto = nombreResto;
+        this.email = email;
+        this.contraseña = contraseña;
+        this.ubicacion = ubicacion;
+        this.numMesas = numMesas;
+        this.diasHorarios = diasHorarios;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombreResto() {
+        return nombreResto;
+    }
+
+    public void setNombreResto(String nombreResto) {
+        this.nombreResto = nombreResto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getNumMesas() {
+        return numMesas;
+    }
+
+    public void setNumMesas(int numMesas) {
+        this.numMesas = numMesas;
+    }
+
+    public LocalDateTime getDiasHorarios() {
+        return diasHorarios;
+    }
+
+    public void setDiasHorarios(LocalDateTime diasHorarios) {
+        this.diasHorarios = diasHorarios;
+    }
 }
